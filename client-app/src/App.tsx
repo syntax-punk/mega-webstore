fb4ecfe83903591f9e28ece65c4959775f02ec31
theVoogie <david.jaeren@webstep.no>
2023-10-13 14:12:04 +0200

    Initial structure of the app and also added CORS service and fixed the middleware

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,6 +1,25 @@
+import { useEffect, useState } from "react"
+
 function App() {
+
+  const [products, setProducts] = useState([])
+
+  useEffect(function fetchProducts() {
+    fetch("http://localhost:5000/api/products")
+      .then((response) => response.json())
+      .then((products) => setProducts(products))
+  }, [])
+
   return (
-    <h1>Hello</h1>
+    <div>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            {product.name} - {product.price}
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
