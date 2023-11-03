13bfb00f15beaef8fae75bd82f0c2806aa096e0f
theVoogie <david.jaeren@webstep.no>
2023-11-03 16:20:58 +0100

    Make AddItemToBasket to return a correct response with Location header in 201. Added AllowCredentials to the cors settings in Program

--- API/Controllers/BasketController.cs
+++ API/Controllers/BasketController.cs
@@ -14,31 +14,17 @@ namespace API.Controllers
             _context = context;
         }
 
-        [HttpGet]
+        [HttpGet(Name = "GetBasket")]
         public async Task<ActionResult<BasketDto>> GetBasket()
         {
             var basket = await RetrieveBasket();
             if (basket == null) return NotFound();
 
-            return new BasketDto
-            {
-                Id = basket.Id,
-                BuyerId = basket.BuyerId,
-                Items = basket.Items.Select(item => new BasketItemDto
-                {
-                    ProductId = item.ProductId,
-                    Name = item.Product.Name,
-                    Price = item.Product.Price,
-                    PictureUrl = item.Product.PictureUrl,
-                    Brand = item.Product.Name,
-                    Type = item.Product.Type,
-                    Quantity = item.Quantity
-                }).ToList()
-            };
+            return MapBasketToDto(basket);
         }
 
         [HttpPost]
-        public async Task<ActionResult> AddItemToBasket(int productId, int quantity) 
+        public async Task<ActionResult<BasketDto>> AddItemToBasket(int productId, int quantity) 
         {
             var basket = await RetrieveBasket();
             if (basket == null) 
@@ -51,7 +37,7 @@ namespace API.Controllers
             
             var result = await _context.SaveChangesAsync() > 0;
 
-            if (result) return StatusCode(201);
+            if (result) return CreatedAtRoute("GetBasket", MapBasketToDto(basket));
 
             return BadRequest(new ProblemDetails{
                 Title = "Problem storing items into basket",
@@ -103,6 +89,25 @@ namespace API.Controllers
             _context.Baskets.Add(basket);
 
             return basket;
+        }
+
+        private BasketDto MapBasketToDto(Basket basket) 
+        {
+            return new BasketDto
+            {
+                Id = basket.Id,
+                BuyerId = basket.BuyerId,
+                Items = basket.Items.Select(item => new BasketItemDto
+                {
+                    ProductId = item.ProductId,
+                    Name = item.Product.Name,
+                    Price = item.Product.Price,
+                    PictureUrl = item.Product.PictureUrl,
+                    Brand = item.Product.Name,
+                    Type = item.Product.Type,
+                    Quantity = item.Quantity
+                }).ToList()
+            };
         }
     }
 }
