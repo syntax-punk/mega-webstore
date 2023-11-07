099327e2a00bfb55aa33aa2681592ff44be7a205
theVoogie <david.jaeren@webstep.no>
2023-11-07 14:03:12 +0100

    Made add and remove items buttons work

--- client-app/src/features/basket/BasketPage.tsx
+++ client-app/src/features/basket/BasketPage.tsx
@@ -1,10 +1,42 @@
-import { IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
-import { Delete } from "@mui/icons-material";
+import { Box, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
+import { Add, Delete, Remove } from "@mui/icons-material";
 import { useStoreContext } from "../../app/context/StoreContext";
+import { agent } from "../../app/api/agent";
+import { useState } from "react";
+import { Basket } from "../../app/models/basket";
+import { LoadingButton } from "@mui/lab";
 
 function BasketPage() {
-  const { basket } = useStoreContext();
+  const { basket, setBasket, removeItem } = useStoreContext();
+  const [ status, setStatus] = useState({
+    loading: false,
+    name: ''
+  });
   
+  function handleAddItem(productId: number, name: string) {
+    setStatus({
+      loading: true,
+      name
+    });
+
+    agent.Basket.addItem(productId)
+      .then((basket: Basket) => setBasket(basket))
+      .catch(error => console.error(error))
+      .finally(() => setStatus({ loading: false, name: '' }));
+  }
+
+  function handleRemoveItem(productId: number, quantity = 1, name: string) {
+    setStatus({
+      loading: true,
+      name
+    });
+
+    agent.Basket.removeItem(productId, quantity)
+      .then(() => removeItem(productId, quantity))
+      .catch(error => console.error(error))
+      .finally(() => setStatus({ loading: false, name: '' }));
+  }
+
   if (!basket) return <Typography variant="h3">Your basket is empty</Typography>
 
   return (
@@ -14,7 +46,7 @@ function BasketPage() {
           <TableRow>
             <TableCell>Product</TableCell>
             <TableCell align="right">Price</TableCell>
-            <TableCell align="right">Quantity</TableCell>
+            <TableCell align="center">Quantity</TableCell>
             <TableCell align="right">Subtotal</TableCell>
             <TableCell align="right"></TableCell>
           </TableRow>
@@ -26,15 +58,42 @@ function BasketPage() {
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
               <TableCell component="th" scope="row">
-                {item.name}
+                <Box display='flex' alignItems='center'>
+                  <img src={item.pictureUrl} alt={item.name} style={{ height: 50,  marginRight: 20 }}/>
+                  <span>{item.name}</span>
+                </Box>
               </TableCell>
               <TableCell align="right">${(item.price / 100).toFixed(2)}</TableCell>
-              <TableCell align="right">{item.quantity}</TableCell>
+              <TableCell align="center">
+                <Box display="flex" justifyContent="center" alignItems="center">
+                  <LoadingButton 
+                    loading={status.loading && status.name === `rem${item.productId}`}
+                    color="primary" 
+                    size="small" 
+                    onClick={() => handleRemoveItem(item.productId, 1, `rem${item.productId}`)}>
+                      <Remove />
+                  </LoadingButton>
+                  <span style={{ fontSize: '16px' }}>
+                    {item.quantity}
+                  </span>
+                  <LoadingButton 
+                    loading={status.loading && status.name === `add${item.productId}`}
+                    color="primary" 
+                    size="small" 
+                    onClick={() => handleAddItem(item.productId, `add${item.productId}`)}>
+                      <Add />
+                  </LoadingButton>
+                </Box>
+              </TableCell>
               <TableCell align="right">${((item.price * item.quantity) / 100).toFixed(2)}</TableCell>
               <TableCell align="right">
-                <IconButton size="small" color="error">
-                  <Delete />
-                </IconButton>
+                <LoadingButton 
+                  loading={status.loading && status.name === `del${item.productId}`}
+                  size="small" 
+                  color="error" 
+                  onClick={() => handleRemoveItem(item.productId, item.quantity, `del${item.productId}`)}>
+                    <Delete />
+                </LoadingButton>
               </TableCell>
             </TableRow>
           ))}
