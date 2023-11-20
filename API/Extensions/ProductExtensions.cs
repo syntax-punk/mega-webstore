2b11d37b31ef0fd261704fad491cb9ff767d6870
syntax-punk <david.jaeren@webstep.no>
2023-11-20 11:26:55 +0100

    Implemented filtering

--- API/Extensions/ProductExtensions.cs
+++ API/Extensions/ProductExtensions.cs
@@ -26,5 +26,22 @@ namespace API.Extensions
                 .Where(p => p.Name.ToLower().Contains(searchTerm.ToLower().Trim()));
             return query;
         }
+
+        public static IQueryable<Product> Filter(this IQueryable<Product> query, string brands, string types) 
+        {
+            var brandList = new List<string>();
+            var typeList = new List<string>();
+
+            if (!string.IsNullOrEmpty(brands))
+                brandList.AddRange(brands.ToLower().Split(",").ToList());
+            
+            if (!string.IsNullOrEmpty(types))
+                typeList.AddRange(types.ToLower().Split(",").ToList());
+            
+            query = query.Where(p => brandList.Count == 0 || brandList.Contains(p.Brand.ToLower()));
+            query = query.Where(p => typeList.Count == 0 || typeList.Contains(p.Type.ToLower()));
+
+            return query;
+        }
     }
 }
