b9c48dd0010942967a057c7af4b2ebe811b39e70
syntax-punk <david.jaeren@gmail.com>
2024-02-19 15:39:26 +0100

    Created order entities and refactored user and role identities to use int instead of guid for Ids

--- API/Data/StoreContext.cs
+++ API/Data/StoreContext.cs
@@ -1,4 +1,5 @@
 using API.Entities;
+using API.Entities.OrderAggregate;
 using Entities;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
@@ -6,7 +7,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
 {
-    public class StoreContext : IdentityDbContext<User>
+    public class StoreContext : IdentityDbContext<User, Role, int>
     {
         public StoreContext(DbContextOptions options) : base(options)
         {
@@ -18,15 +19,22 @@ namespace API.Data
 
         public DbSet<Product> Products { get; set; }
         public DbSet<Basket> Baskets { get; set; }
+        public DbSet<Order> Orders { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
 
-            builder.Entity<IdentityRole>()
+            builder.Entity<User>()
+                .HasOne(u => u.Address)
+                .WithOne()
+                .HasForeignKey<UserAddress>(a => a.Id)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<Role>()
                 .HasData(
-                    new IdentityRole { Name = "Admin", NormalizedName = "ADMIN" },
-                    new IdentityRole { Name = "Member", NormalizedName = "MEMBER" }
+                    new Role { Id = 1, Name = "Admin", NormalizedName = "ADMIN" },
+                    new Role { Id = 2, Name = "Member", NormalizedName = "MEMBER" }
                 );
         }
     }
