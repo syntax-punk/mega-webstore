ebba83f7d03d73d55c15a395ab189ddd96e04086
syntax-punk <david.jaeren@gmail.com>
2024-03-18 16:32:22 +0100

    Added stripe and filled appsettings with secret

--- API/Entities/Basket.cs
+++ API/Entities/Basket.cs
@@ -7,33 +7,35 @@ namespace API.Entities
         public int Id { get; set; }
         public string BuyerId { get; set; }
         public List<BasketItem> Items { get; set; } = new();
-
+        public string PaymentIntentId { get; set; }
+        public string ClientSecret { get; set; }
         public void AddItem(Product product, int quantity = 1)
         {
             var existingItem = Items.FirstOrDefault(item => item.ProductId == product.Id);
 
-            if (existingItem == null) {
+            if (existingItem == null)
+            {
                 Items.Add(new BasketItem
                 {
                     Product = product,
                     Quantity = quantity
                 });
-            } 
-            else 
+            }
+            else
             {
                 existingItem.Quantity += quantity;
             }
         }
 
-        public void RemoveItem(int productId, int quantity = 1) 
+        public void RemoveItem(int productId, int quantity = 1)
         {
             var existingItem = Items.FirstOrDefault(item => item.ProductId == productId);
             if (existingItem == null) return;
 
-            if (existingItem.Quantity > quantity) 
+            if (existingItem.Quantity > quantity)
             {
                 existingItem.Quantity -= quantity;
-            } 
+            }
             else
             {
                 Items.Remove(existingItem);
