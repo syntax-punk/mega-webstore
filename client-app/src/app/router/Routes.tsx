2f1c6979840aa8496491bc204cb8b393fa3f1360
syntax-punk <david.jaeren@gmail.com>
2024-03-29 18:33:22 +0100

    Prepping the production build

--- client-app/src/app/router/Routes.tsx
+++ client-app/src/app/router/Routes.tsx
@@ -1,6 +1,5 @@
 import { Navigate, createBrowserRouter } from "react-router-dom";
 import App from "../layout/App";
-import { HomePage } from "../../features/home/HomePage";
 import { Catalog } from "../../features/catalog/Catalog";
 import { ProductDetails } from "../../features/catalog/ProductDetails";
 import { AboutPage } from "../../features/about/AboutPage";
