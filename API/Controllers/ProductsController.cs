c927143a2dff7d7a41a3be7b70a73456c6cacdd1
syntax-punk <david.jaeren@gmail.com>
2024-04-05 14:08:56 +0200

    Added update operation for updating product info

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 using API.Data;
 using API.DTOs;
+using API.Entities;
 using API.Extensions;
 using API.RequestHelpers;
 using AutoMapper;
@@ -76,6 +77,26 @@ namespace API.Controllers
             if (result) return CreatedAtRoute("GetProduct", new { Id = product.Id }, product);
 
             return BadRequest(new ProblemDetails { Title = "Problem creating new product" });
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPut]
+        public async Task<ActionResult> UpdateProduct(UpdateProductDto productDto)
+        {
+            var product = await _context.Products.FindAsync(productDto.Id);
+
+            if (product == null) return NotFound();
+
+            _mapper.Map(productDto, product);
+
+            var result = await _context.SaveChangesAsync() > 0;
+
+            if (result) return NoContent();
+
+            return BadRequest(new ProblemDetails
+            {
+                Title = "We have an issue with updating product changes"
+            });
         }
     }
 }
