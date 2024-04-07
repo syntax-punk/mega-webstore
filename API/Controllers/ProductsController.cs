fc0acedd157d5140895424775dd8fb4c04e5c7cf
syntax-punk <david.jaeren@gmail.com>
2024-04-07 18:25:05 +0200

    Updated controller to upload images to clopudinary

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -3,6 +3,7 @@ using API.DTOs;
 using API.Entities;
 using API.Extensions;
 using API.RequestHelpers;
+using API.Services;
 using AutoMapper;
 using Entities;
 using Microsoft.AspNetCore.Authorization;
@@ -15,11 +16,13 @@ namespace API.Controllers
     {
         private readonly StoreContext _context;
         private readonly IMapper _mapper;
+        private readonly ImageService _imageService;
 
-        public ProductsController(StoreContext context, IMapper mapper)
+        public ProductsController(StoreContext context, IMapper mapper, ImageService imageService)
         {
             _mapper = mapper;
             _context = context;
+            _imageService = imageService;
         }
 
         [HttpGet]
@@ -67,9 +70,26 @@ namespace API.Controllers
 
         [Authorize(Roles = "Admin")]
         [HttpPost]
-        public async Task<ActionResult<Product>> CreateProduct(CreateProductDto productDto)
+        public async Task<ActionResult<Product>> CreateProduct([FromForm] CreateProductDto productDto)
         {
             var product = _mapper.Map<Product>(productDto);
+
+            if (productDto.File != null)
+            {
+                var imageResult = await _imageService.AddImageAsync(productDto.File);
+
+                if (imageResult.Error != null)
+                {
+                    return BadRequest(new ProblemDetails
+                    {
+                        Title = imageResult.Error.Message
+                    });
+                }
+
+                product.PictureUrl = imageResult.SecureUrl.ToString();
+                product.ImagePublicId = imageResult.PublicId;
+            }
+
 
             _context.Products.Add(product);
             var result = await _context.SaveChangesAsync() > 0;
