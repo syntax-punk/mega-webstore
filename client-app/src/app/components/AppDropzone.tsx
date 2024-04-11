03d69f7c125c8d1709bdcb624f0b87d06b03fac6
syntax-punk <david.jaeren@gmail.com>
2024-04-11 16:15:10 +0200

    Added AppDropzone component

---
+++ client-app/src/app/components/AppDropzone.tsx
@@ -0,0 +1,30 @@
+import {useCallback} from 'react'
+import {useDropzone} from 'react-dropzone'
+import { UseControllerProps, useController } from 'react-hook-form'
+
+interface Props extends UseControllerProps {}
+
+export function AppDropzone(props: Props) {
+  const { fieldState, field } = useController({...props, defaultValue: ''});
+
+  const onDrop = useCallback((acceptedFiles: any) => {
+    acceptedFiles[0] = Object.assign(acceptedFiles[0], {
+      preview: URL.createObjectURL(acceptedFiles[0])
+    });
+
+    field.onChange(acceptedFiles[0]);
+  }, [field]);
+
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
+  return (
+    <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      {
+        isDragActive ?
+          <p>Drop the files here ...</p> :
+          <p>Drag 'n' drop some files here, or click to select files</p>
+      }
+    </div>
+  )
+}
--- client-app/src/features/admin/ProductForm.tsx
+++ client-app/src/features/admin/ProductForm.tsx
@@ -1,10 +1,11 @@
 import { Typography, Grid, Paper, Box, Button } from "@mui/material";
-import { useForm } from "react-hook-form";
+import { FieldValues, useForm } from "react-hook-form";
 import { AppTextInput } from "../../app/components/AppTextInput";
 import { Product } from "../../app/models/product";
 import { useEffect } from "react";
 import { useProducts } from "../../app/hooks/useProducts";
 import { AppSelectList } from "../../app/components/AppSelectList";
+import { AppDropzone } from "../../app/components/AppDropzone";
 
 interface Props {
   cancelEdit: VoidFunction;
@@ -12,45 +13,51 @@ interface Props {
 }
 
 export function ProductForm({ cancelEdit, product }: Props) {
-    const { control, reset } = useForm();
+    const { control, reset, handleSubmit } = useForm();
     const { brands, types } = useProducts();
 
     useEffect(() => {
       if (product) reset(product);
     }, [product, reset]);
 
+    function handleSubmitData(data: FieldValues) {
+      console.log('-> ', data);
+    }
+
     return (
         <Box component={Paper} sx={{p: 4}}>
             <Typography variant="h4" gutterBottom sx={{mb: 4}}>
                 Product Details
             </Typography>
-            <Grid container spacing={3}>
-                <Grid item xs={12} sm={12}>
-                    <AppTextInput control={control} name='name' label='Product name' />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                    <AppSelectList items={brands} control={control} name='brand' label='Brand' />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                    <AppSelectList items={types} control={control} name='type' label='Type' />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                    <AppTextInput type="number" control={control} name='price' label='Price' />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                    <AppTextInput type="number" control={control} name='quantityInStock' label='Quantity in Stock' />
-                </Grid>
-                <Grid item xs={12}>
-                    <AppTextInput multiline={true} rows={4} control={control} name='description' label='Description' />
-                </Grid>
-                <Grid item xs={12}>
-                    <AppTextInput control={control} name='pictureUrl' label='Image' />
-                </Grid>
-            </Grid>
-            <Box display='flex' justifyContent='space-between' sx={{mt: 3}}>
-                <Button onClick={cancelEdit} variant='contained' color='inherit'>Cancel</Button>
-                <Button variant='contained' color='success'>Submit</Button>
-            </Box>
+            <form onSubmit={handleSubmit(handleSubmitData)}>
+              <Grid container spacing={3}>
+                  <Grid item xs={12} sm={12}>
+                      <AppTextInput control={control} name='name' label='Product name' />
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
+                      <AppSelectList items={brands} control={control} name='brand' label='Brand' />
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
+                      <AppSelectList items={types} control={control} name='type' label='Type' />
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
+                      <AppTextInput type="number" control={control} name='price' label='Price' />
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
+                      <AppTextInput type="number" control={control} name='quantityInStock' label='Quantity in Stock' />
+                  </Grid>
+                  <Grid item xs={12}>
+                      <AppTextInput multiline={true} rows={4} control={control} name='description' label='Description' />
+                  </Grid>
+                  <Grid item xs={12}>
+                      <AppDropzone control={control} name='file' />
+                  </Grid>
+              </Grid>
+              <Box display='flex' justifyContent='space-between' sx={{mt: 3}}>
+                  <Button onClick={cancelEdit} variant='contained' color='inherit'>Cancel</Button>
+                  <Button type="submit" variant='contained' color='success'>Submit</Button>
+              </Box>
+            </form>
         </Box>
     )
 }
