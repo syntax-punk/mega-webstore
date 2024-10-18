95261b086865d39883724e56f463a77a94c5f91a
syntax-punk <david.jaeren@gmail.com>
2024-10-18 22:55:41 +0200

    Updated the look and feel of the main screen

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -1,40 +1,75 @@
 import { Box, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
 import Slider from "react-slick";
 
+function SamplePrevArrow() {
+  return (
+    <div/>
+  );
+}
+
 function HomePage() {
-  const settings = {
+
+  const navStyles = {
+    color: 'inherit',
+    textDecoration: 'underline',
+    typography: 'h5',
+    '&:hover': {
+      color: 'grey.500'
+    },
+    '&.active': {
+      color: 'text.secondary'
+    }
+  }
+
+  const sliderSettings = {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 1,
+    slidesToShow: 3,
     slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    pauseOnHover: true,
+    cssEase: "linear",
+    nextArrow: <SamplePrevArrow />,
+    prevArrow: <SamplePrevArrow />,
   };
+
+  const images = [
+    "/images/hero1.png",
+    "/images/hero2.png",
+    "/images/hero3.png",
+    "/images/hero4.png",
+    "/images/hero5.png",
+    "/images/hero6.png",
+    "/images/hero7.png",
+    "/images/hero8.png",
+    "/images/hero9.png",
+    "/images/hero10.png",
+    "/images/hero11.png",
+    "/images/hero12.png",
+    "/images/hero13.png",
+  ];
 
   return (
     <>
-      <Slider {...settings}>
-        <div>
-          <img 
-            src="/images/hero1.jpg" 
-            alt="Hero image" 
-            style={{ display: "block", width: "100%", maxHeight: 500 }} />
-        </div>
-        <div>
-          <img 
-            src="/images/hero2.jpg" 
-            alt="Hero image" 
-            style={{ display: "block", width: "100%", maxHeight: 500 }} />
-        </div>
-        <div>
-          <img 
-            src="/images/hero3.jpg" 
-            alt="Hero image" 
-            style={{ display: "block", width: "100%", maxHeight: 500 }} />
-        </div>
+      <Slider {...sliderSettings}>
+        {images.sort(() => Math.random() - 0.5).map((image, index) => (
+          <div key={index}>
+            <img 
+              src={image} 
+              alt="Hero image" 
+              style={{ display: "block", margin: "auto", maxHeight: 500 }} />
+          </div>
+        ))}
       </Slider>
-      <Box display="flex" justifyContent="center" sx={{ p:4 }}>
-        <Typography variant="h1">
-          Welcome to the shop
+      <Box display="flex" justifyContent="center" alignItems="center" flexDirection="column" gap="1rem" sx={{ p:4 }}>
+        <Typography variant="h2">
+          welcome to the megastore
+        </Typography>
+        <Typography component={Link} to="/catalog" sx={navStyles} > 
+          go to the catalog
         </Typography>
       </Box>
     </>
