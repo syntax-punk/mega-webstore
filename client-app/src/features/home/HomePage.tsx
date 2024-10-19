95f9903d8cac3696b1d196e29739fbcc38936497
syntax-punk <david.jaeren@gmail.com>
2024-10-19 12:41:39 +0200

    Updated HomePage to the point where I am kinda ok with how it looks

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -1,6 +1,7 @@
 import { Box, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 import Slider from "react-slick";
+import "./HomePage.css";
 
 function SamplePrevArrow() {
   return (
@@ -12,8 +13,8 @@ function HomePage() {
 
   const navStyles = {
     color: 'inherit',
-    textDecoration: 'underline',
-    typography: 'h5',
+    textDecoration: 'none',
+    typography: 'h4',
     '&:hover': {
       color: 'grey.500'
     },
@@ -22,20 +23,32 @@ function HomePage() {
     }
   }
 
-  const sliderSettings = {
+  const topSliderSettings = {
     infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
+    slidesToShow: 4,
+    slidesToScroll: 2,
     autoplay: true,
     speed: 500,
-    autoplaySpeed: 2000,
+    autoplaySpeed: 3000,
     pauseOnHover: true,
     cssEase: "linear",
     nextArrow: <SamplePrevArrow />,
     prevArrow: <SamplePrevArrow />,
   };
 
-  const images = [
+  const bottomSliderSettings = {
+    infinite: true,
+    slidesToShow: 8,
+    slidesToScroll: 4,
+    autoplay: true,
+    rtl: true,
+    speed: 10000,
+    autoplaySpeed: 500,
+    nextArrow: <SamplePrevArrow />,
+    prevArrow: <SamplePrevArrow />,
+  };
+
+  const topRowImages = [
     "/images/hero1.png",
     "/images/hero2.png",
     "/images/hero3.png",
@@ -51,15 +64,38 @@ function HomePage() {
     "/images/hero13.png",
   ];
 
+  const bottomRowImages = [
+    "/images/products/boot-ang1.png",
+    "/images/products/boot-ang2.png",
+    "/images/products/boot-core1.png",
+    "/images/products/boot-core2.png",
+    "/images/products/boot-redis1.png",
+    "/images/products/glove-code1.png",
+    "/images/products/glove-code2.png",
+    "/images/products/glove-react1.png",
+    "/images/products/glove-react2.png",
+    "/images/products/helm-core1.png",
+    "/images/products/helm-react1.png",
+    "/images/products/helm-react2.png",
+    "/images/products/helm-react3.png",
+    "/images/products/helm-react4.png",
+    "/images/products/sb-ang1.png",
+    "/images/products/sb-ang2.png",
+    "/images/products/sb-core1.png",
+    "/images/products/sb-core2.png",
+    "/images/products/sb-react1.png",
+    "/images/products/sb-ts1.png",
+  ];
+
   return (
     <>
-      <Slider {...sliderSettings}>
-        {images.sort(() => Math.random() - 0.5).map((image, index) => (
+      <Slider {...topSliderSettings}>
+        {topRowImages.sort(() => Math.random() - 0.5).map((image, index) => (
           <div key={index}>
             <img 
               src={image} 
               alt="Hero image" 
-              style={{ display: "block", margin: "auto", maxHeight: 500 }} />
+              style={{ display: "block", margin: "auto", maxHeight: 400 }} />
           </div>
         ))}
       </Slider>
@@ -68,9 +104,20 @@ function HomePage() {
           megastore
         </Typography>
         <Typography component={Link} to="/catalog" sx={navStyles} > 
-          go to the catalog
+          catalog
         </Typography>
       </Box>
+      <Slider {...bottomSliderSettings}>
+        {bottomRowImages.sort(() => Math.random() - 0.5).map((image, index) => (
+          <div key={index}>
+            <img 
+              src={image} 
+              alt="Hero image" 
+              style={{ display: "block", margin: "auto", maxHeight: 280 }}  
+            />
+          </div>
+        ))}
+      </Slider>
     </>
   );
 }
