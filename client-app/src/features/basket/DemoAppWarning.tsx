8cecafb6b8b52664d2db6deee8cf9338a6b1b793
syntax-punk <david.jaeren@gmail.com>
2024-10-21 10:17:22 +0200

    Fixed a bug in the dialog form

--- client-app/src/features/basket/DemoAppWarning.tsx
+++ client-app/src/features/basket/DemoAppWarning.tsx
@@ -3,14 +3,21 @@ import { useState } from 'react'
 
 export default function DemoAppWarning() {
   const [openDialog, setOpenDialog] = useState(true);
+  const boxStyles = { 
+    position: "fixed", 
+    top: "25%", 
+    border: "12px solid red", 
+    backgroundColor: "#FFF", 
+    boxShadow: "12px 12px black, -12px -12px black" 
+  } as React.CSSProperties;
   
   return (
-    <dialog open={openDialog} style={{ position: "fixed", top: "25%", border: "8px solid red" }}>
+    <dialog open={openDialog} style={boxStyles}>
       <Box>
         <Typography variant="h3">Remember</Typography>
         <Typography variant="h3">This is a DEMO app!</Typography>
-        <Typography variant="h3" paddingBottom={2}>DO NOT USE YOUR REAL BANKCARD!</Typography>
-        <Button size="large" variant="contained" onClick={() => setOpenDialog(false)}>I understand</Button>
+        <Typography variant="h3" paddingBottom={4}>DO NOT USE YOUR REAL BANKCARD!</Typography>
+        <Button size="large" variant="contained" fullWidth onClick={() => setOpenDialog(false)}>I understand</Button>
       </Box>
   </dialog>
   )
